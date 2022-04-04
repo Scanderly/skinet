1b48ebeacf2830596d42726ed5069af577208674
2ra190scan@gmail.com <2ra190scan@gmail.com>
2022-04-04 23:26:11 +0400

    Initial commit

---
+++ Infrastructure/Data/StoreContent.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; 
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class StoreContent:DbContext
+    {
+        public StoreContent(DbContextOptions<StoreContent> options):base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        
+        
+    }
+}
